b7794654068f63612f08758ea37e50900f5cc88a
trang32 <94185643+trang32@users.noreply.github.com>
2022-12-08 17:12:00 +0100

    finish neural network

---
+++ trang_projetS3/main.c
@@ -0,0 +1,36 @@
+#include <math.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "util/img.h"
+#include "neural/activations.h"
+#include "neural/nn.h"
+#include "matrix/matrix.h"
+#include "matrix/ops.h"
+
+int main() {
+	srand(time(NULL));
+
+	//TRAINING
+	// int number_imgs = 10000;
+	// Img** imgs = csv_to_imgs("data/mnist_train.csv", number_imgs);
+	// NeuralNetwork* net = network_create(784, 300, 10, 0.1);
+	// network_train_batch_imgs(net, imgs, number_imgs);
+	// network_save(net, "testing_net");
+
+	// PREDICTING
+	// int number_imgs = 3000;
+	// Image** imgs = csv_to_imgs("data/mnist_test.csv", number_imgs);
+	// NeuralNetwork* net = network_load("testing_net");
+
+	//Matrix* res = network_predict_img(net,imgs[0]);
+	//printf("Network Predicted %d", matrix_argmax(res));
+
+
+	// double score = network_predict_imgs(net, imgs, 1000);
+	// printf("Score: %1.5f", score);
+
+	// imgs_free(imgs, number_imgs);
+	// network_free(net);
+	return 0;
+}
